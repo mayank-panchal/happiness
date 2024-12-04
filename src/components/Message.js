4da0ac0f7d28f294a3c0bae2ac9e256f0fa0a83f
Mayank Panchal <mayankpanchal1305@gmail.com>
2024-12-04 12:21:06 +0530

    Update changes

--- src/components/Message.js
+++ src/components/Message.js
@@ -11,7 +11,8 @@ function Message() {
         He ek gift mazhyakadna ki tu kiti amazing ani special aahes and u deserve better than just a website bhai u deserve biography ani ofc me asen tya biography cha writer hehe.
         Cause I love to do anything which is related to my beautiful loving woman. Aajcha ha divas, tuzha 18th birthday khuppp changla asava not just ha divas ha tuzha yenar cricket career as well ur life, saglaaaaaaaa mastaaaaa houdeee ashi mazhi devakade prarthana aaheeee.
         Tu jevda ektine counter kelay tevda konich ny karu shakat. Tu khup tough aahes yaar. Fighting spirit aahe tuzhyakade. Pn kdhi vatla fight nahi karavasa then dont worry me asnar tuzhyasobat tuzha saath dyayla. 
-        Kayam asnar tuzhyasobat ani chehryavar ek cute shi smile bghayla khup khup avadta.
+        Kayam asnar tuzhyasobat ani chehryavar ek cute shi smile bghayla khup khup avadta. I love you yar vaishu. Eka cuteeeee lahannn mulgiii jilaa faktaa abhyas karayla avdaycha, te aata ek sundar smart lady, jila cricket shivay khich nahi avdat. You have grown up babe. Ani irl aajun heights tula pohochaychay. Ani ho tu te karu shakshil.
+        Aajcha ha divas ek asa new beginning mhnun samaj tuzhyasathii. Ya nntr saglaaa mastaa jailll. yaahuuuuuuuuuuuuuuu vaishuuuu chaaaaaa birthdayyyyyyyy. toooo pnnnn 18thhhh!!!!!!!!!
         Here's to a day as amazing as you are. I Love you! 💖
       </p>
     </section>
